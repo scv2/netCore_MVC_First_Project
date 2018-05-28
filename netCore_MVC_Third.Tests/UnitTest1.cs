33a8eb9ab4efbe513369ae73a8ef7021804be656
Chintan Vyas <chintankumar.vyas@oncor.com>
2018-05-28 17:52:28 -0500

    Adding first net Core MVC project

---
+++ netCore_MVC_Third.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace netCore_MVC_Third.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ netCore_MVC_Third/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netCore_MVC_Third.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
